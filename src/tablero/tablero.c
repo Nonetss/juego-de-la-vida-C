7e67393d504d666ee3c6121f37e18d872925a062
Nonetss <amorenolopezbarajas@gmail.com>
2024-12-16 14:17:22 +0100

    Introducimos la funcion para saber si queda alguna celula viva

--- src/tablero/tablero.c
+++ src/tablero/tablero.c
@@ -1,39 +1,50 @@
+#include "tablero.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "tablero.h"
 
 // Crear una matriz dinámica
 int **crearTablero(int filas, int columnas) {
-    int **tablero = (int **)malloc(filas * sizeof(int *));
-    for (int i = 0; i < filas; i++) {
-        tablero[i] = (int *)malloc(columnas * sizeof(int));
-    }
-    return tablero;
+  int **tablero = (int **)malloc(filas * sizeof(int *));
+  for (int i = 0; i < filas; i++) {
+    tablero[i] = (int *)malloc(columnas * sizeof(int));
+  }
+  return tablero;
 }
 
 // Inicializar la matriz con valores aleatorios
 void inicializarTablero(int **tablero, int filas, int columnas) {
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
-            tablero[i][j] = rand() % 2;
-        }
+  for (int i = 0; i < filas; i++) {
+    for (int j = 0; j < columnas; j++) {
+      tablero[i][j] = rand() % 2;
     }
+  }
 }
 
 // Imprimir el tablero
 void imprimirTablero(int **tablero, int filas, int columnas) {
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
-            printf("%d ", tablero[i][j]);
-        }
-        printf("\n");
+  for (int i = 0; i < filas; i++) {
+    for (int j = 0; j < columnas; j++) {
+      printf("%d ", tablero[i][j]);
     }
+    printf("\n");
+  }
 }
 
 // Liberar la memoria asignada al tablero
 void liberarTablero(int **tablero, int filas) {
-    for (int i = 0; i < filas; i++) {
-        free(tablero[i]);
+  for (int i = 0; i < filas; i++) {
+    free(tablero[i]);
+  }
+  free(tablero);
+}
+
+int hayCelulasVivas(int **tablero, int filas, int columnas) {
+  for (int x = 0; x < filas; x++) {
+    for (int y = 0; y < columnas; y++) {
+      if (tablero[x][y] == 1) {
+        return 1; // Hay al menos una célula viva
+      }
     }
-    free(tablero);
+  }
+  return 0; // No hay células vivas
 }
--- src/tablero/tablero.h
+++ src/tablero/tablero.h
@@ -6,5 +6,6 @@ int **crearTablero(int filas, int columnas);
 void inicializarTablero(int **tablero, int filas, int columnas);
 void imprimirTablero(int **tablero, int filas, int columnas);
 void liberarTablero(int **tablero, int filas);
+int hayCelulasVivas(int **tablero, int filas, int columnas);
 
 #endif // TABLERO_H
